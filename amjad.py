ebef17018c485713de97b574ae4b10116414a48a
amjadadhie <prasetyoamjad@gmail.com>
2024-04-24 02:18:44 +0700

    Create amjad.py

---
+++ amjad.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# Memuat data
+train_features = pd.read_csv("dataset/train_features.csv")
+train_labels = pd.read_csv("dataset/train_labels.csv")
+test_features = pd.read_csv("dataset/test_features.csv")
